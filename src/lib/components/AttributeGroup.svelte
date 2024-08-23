827b10b2086f82372e1f37dd0cc0365a79c1639e
Azhar Rizqullah <azharizqullah90@gmail.com>
2024-08-23 21:47:57 +0800

    feat: change how AttributeGroup is positioned when opened

--- src/lib/components/AttributeGroup.svelte
+++ src/lib/components/AttributeGroup.svelte
@@ -8,16 +8,26 @@
     let newAttribute = "";
     let linkAlias = "";
 
-    function showAddPopover(e: MouseEvent): void {
-        if (!e.clientX && !e.clientY) {
-            dialog.style.top = "50%";
-            dialog.style.left = "50%";
-            dialog.style.transform = "translate(-50%, -50%)";
-        } else {
-            dialog.style.top = e.clientY + "px";
-            dialog.style.left = e.clientX + "px";
-        }
+    let showPopoverButtonEl: HTMLButtonElement;
+
+    function updatePosition(): void {
+        let { x, y } = showPopoverButtonEl.getBoundingClientRect();
+
+        const popoverWidth = dialog.clientWidth;
+        const popoverHeight = dialog.clientHeight;
+        const buttonWidth = showPopoverButtonEl.clientWidth;
+        const buttonHeight = showPopoverButtonEl.clientHeight;
+
+        x = x + buttonWidth - popoverWidth / 2;
+        y = y - popoverHeight;
+
+        dialog.style.left = x < 0 ? 0 + "px" : x + "px";
+        dialog.style.top = y < 0 ? y + popoverHeight + buttonHeight + "px" : y + "px";
+    }
+
+    function showAddPopover(): void {
         dialog.showPopover();
+        updatePosition();
     }
 
     function addHandler(): void {
@@ -46,29 +56,46 @@
     }
 </script>
 
+<svelte:window on:resize={updatePosition} />
+
 <section class="flex gap-4 bg-ctp-surface1 px-6">
-    <svelte:component class="flex-shrink-0" this={attributeType === "TAG" ? Tag : Link} />
+    <svelte:component this={attributeType === "TAG" ? Tag : Link} class="flex-shrink-0" />
     <div class="flex flex-wrap {attributeType === 'TAG' ? 'gap-2' : 'gap-4'} items-center">
         {#each attributes as attribute}
             {#if attributeType === "TAG"}
-                <div class="rounded-full bg-ctp-red px-2 text-sm font-bold text-ctp-crust cursor-default flex items-center gap-1">
+                <div
+                    class="flex cursor-default items-center gap-1 rounded-full bg-ctp-red px-2 text-sm font-bold text-ctp-crust"
+                >
                     {attribute}
-                    <button on:click|preventDefault={() => attributes = attributes.filter(attr => attr !== attribute)}>
-                       <X class="w-[1em] h-[1em]"/>
+                    <button
+                        on:click|preventDefault={() =>
+                            (attributes = attributes.filter((attr) => attr !== attribute))}
+                    >
+                        <X class="h-[1em] w-[1em]" />
                     </button>
                 </div>
             {:else if attributeType === "LINK"}
                 <div class="flex items-center gap-1">
-                    <a href={attribute.split(" ")[0]} class="underline decoration-[0.1em] underline-offset-2 hover:text-white">
-                       {attribute.split(" ").at(-1)}
+                    <a
+                        href={attribute.split(" ")[0]}
+                        class="underline decoration-[0.1em] underline-offset-2 hover:text-white"
+                    >
+                        {attribute.split(" ").at(-1)}
                     </a>
-                    <button on:click|preventDefault={() => attributes = attributes.filter(attr => attr !== attribute)}>
-                       <X class="w-[1em] h-[1em]"/>
+                    <button
+                        on:click|preventDefault={() =>
+                            (attributes = attributes.filter((attr) => attr !== attribute))}
+                    >
+                        <X class="h-[1em] w-[1em]" />
                     </button>
                 </div>
             {/if}
         {/each}
-        <button on:click|preventDefault={showAddPopover} class="hover:text-white self-stretch">
+        <button
+            bind:this={showPopoverButtonEl}
+            on:click|preventDefault={showAddPopover}
+            class="self-stretch hover:text-white"
+        >
             <Plus size="16" />
         </button>
         <dialog
@@ -84,7 +111,9 @@
                         type="text"
                         bind:value={newAttribute}
                         on:keydown={handleEnter}
-                        placeholder={attributeType === "TAG" ? "Add a new tag..." : "Add a new link..."}
+                        placeholder={attributeType === "TAG"
+                            ? "Add a new tag..."
+                            : "Add a new link..."}
                         class="bg-ctp-surface0 px-4 py-2 focus:outline-none"
                     />
                     {#if attributeType === "LINK"}
@@ -98,7 +127,7 @@
                     {/if}
                 </div>
                 <button
-                    on:click|preventDefault={() => addHandler()}
+                    on:click|preventDefault={addHandler}
                     class="mr-2 text-ctp-text hover:text-white"
                 >
                     <Plus size="16" />
